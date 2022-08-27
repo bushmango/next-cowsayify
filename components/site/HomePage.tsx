7615bd195d1b9e3fc842642623fa8e0f33a009f8
Unknown <bushmango@gmail.com>
2022-08-27 15:54:15 -0500

    remove dead files, update news

--- components/site/HomePage.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { Layout } from '../layout/Layout'
-
-export const HomePage = () => {
-  return <Layout title='Cowsayify - Say it with cows!'>todo </Layout>
-}
