ccab13c0772e2f98fa65f93e06e8b206e8374a63
Steve Bushman <steve@stevebushman.com>
2022-08-29 23:36:45 -0500

    fix darkmode crash on no localstorage

--- next-core/util/jotaiNextjs.ts
+++ next-core/util/jotaiNextjs.ts
@@ -11,9 +11,25 @@ export function useNextjsAtomWithStorage<T>(key: string, _default: T) {
   return isServer
     ? atom(_default)
     : atomWithStorage(key, _default, {
-        getItem: (key) => JSON.parse(localStorage.getItem(key) || '') as T,
-        setItem: (key, value) =>
-          localStorage.setItem(key, JSON.stringify(value)),
+        getItem: (key) => {
+          let item = _default
+          let itemJson = localStorage.getItem(key) || ''
+          if (itemJson) {
+            try {
+              item = JSON.parse(itemJson) as T
+            } catch (e) {
+              console.error('useNextjsAtomWithStorage get', e)
+            }
+          }
+          return item
+        },
+        setItem: (key, value) => {
+          try {
+            localStorage.setItem(key, JSON.stringify(value))
+          } catch (e) {
+            console.error('useNextjsAtomWithStorage set', e)
+          }
+        },
         removeItem: (key) => localStorage.removeItem(key),
         delayInit: true,
       })
